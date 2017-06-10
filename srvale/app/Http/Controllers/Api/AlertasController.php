5b048cccfca602c96ff2618ae15d968b26fbf657
Mariane Monteiro Quirino <marianemq@ucl.com>
2017-06-09 22:42:56 -0300

    Ajustes na data do api alertas controller

--- srvale/app/Http/Controllers/Api/AlertasController.php
+++ srvale/app/Http/Controllers/Api/AlertasController.php
@@ -51,7 +51,7 @@ class AlertasController extends Controller
 
         $alerta -> cliente_id = $data['cliente_id'];
         $alerta -> prioridade = 2;
-        $alerta -> data_criacao = date('d/m/Y');
+        $alerta -> data_criacao = date('y-m-a');
         $alerta -> qtdaprovadores = 0;
         $alerta -> tipoalerta_id = $data['tipoalerta_id'];
         $alerta -> statusalerta_id = 1;
